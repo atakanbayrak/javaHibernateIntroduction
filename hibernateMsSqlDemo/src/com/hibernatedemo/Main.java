f3aa2d335a3ecf97d9985509d422744bab6bf316
Atakan Bayrak <74923491+atakanbayrak@users.noreply.github.com>
2022-07-21 13:31:10 +0300

    Add files via upload

--- hibernateMsSqlDemo/src/com/hibernatedemo/Main.java
+++ hibernateMsSqlDemo/src/com/hibernatedemo/Main.java
@@ -16,18 +16,49 @@ public class Main {
 		Session session = factory.getCurrentSession();
 		
 		try {
+			//Unit of Work ----> Design Pattern // Baþarýsýz bir iþlem öncekini de iptal ettirir
 			session.beginTransaction();
+			
 			//HQL ----> Hibernate Query Language
 			//Select * from city
 			//from City c where c.countryCode='TUR' AND c.district='Ankara'
 			//from City c where c.name LIKE '%kar%' 
-			List<City> cities = session.createQuery("from City c ").getResultList();
+			//ASC- Ascending
+			//DESC- Descending
 			
-			for (City city : cities) {
-				System.out.println(city.getName());
-			}
+			//SELECT
+			/*
+			 * List<String> countryCodes=
+			 * session.createQuery("select c.countryCode from City c GROUP BY c.countryCode"
+			 * ).getResultList();
+			 * 
+			 * for (String countryCode : countryCodes) { System.out.println(countryCode); }
+			 */
+			
+			//INSERT
+			/*
+			 * City city = new City(); city.setName("Düzce 10"); city.setCountryCode("TUR");
+			 * city.setDistrict("Düzce"); city.setPopulation(10000);
+			 * 
+			 * session.save(city);
+			 * System.out.println("Þehir Eklendi");
+			 */
+			
+			//UPDATE
+			/*
+			 * City city = session.get(City.class, 4086); city.setPopulation(150000);
+			 * session.save(city);
+			 * System.out.println("Þehir Güncellendi");
+			 */
+			
+			//DELETE
+			/*
+			 * City city = session.get(City.class, 4086); session.delete(city);
+			 * System.out.println("Þehir Silindi");
+			 */
 			
 			session.getTransaction().commit();
+			
 		}
 		finally{
 			factory.close();
